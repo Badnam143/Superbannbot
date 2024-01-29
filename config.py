6d8bc4e5a608beb70573d9fd87e09dbb89d13d6f
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2024-01-29 12:06:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     API_ID =   #  just fill your api id here 
     API_HASH = "" #api hash
     BOT_TOKEN = "" #token of your bot get it from @BotFather
-    BAN_PROTECTED = [5285561060] #users who can perform any cmds and also they are ban protected
+    BAN_PROTECTED = [6980223718] #users who can perform any cmds and also they are ban protected
     OWNER_ID =  #id of the owner note :- only owner can broadcast message 
     START_PIC = "" #not necessary 
     MONGO_DB_URL = "" #url of your mongodb
